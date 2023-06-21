15f3a545d4c9fba677f56490cf14ef34417e6d12
emrekiz0 <emreekiz90@gmail.com>
2023-06-21 12:00:57 +0300

    Proje dosyası ekle.

---
+++ Services/BookService/Book.Infastructure/Services/BookService.cs
@@ -0,0 +1,33 @@
+﻿using Book.Domain.Models;
+using Book.Domain.Services;
+
+namespace Book.Infastructure.Services
+{
+    public class BookService : IBookService
+    {
+        public BookDTO GetBookByISBN(string isbn)
+        {
+            if (string.IsNullOrEmpty(isbn))
+            {
+                return null;
+            }
+            var book = new BookDTO
+            {
+                Author = "J.R.R Tolkien",
+                ISBN = isbn,
+                PageCount = 1012,
+                PublisherName = "Metis Yayınevi",
+                Title = "Yüzüklerin Efendisi Tek Cilt Özel Basım"
+            };
+            var book1 = new BookDTO
+            {
+                Author = "Reşat Nuri Gültekin",
+                ISBN = isbn,
+                PageCount = 1012,
+                PublisherName = "Ankara Yayınevi",
+                Title = "Yaprak Dökümü"
+            };
+            return book;
+        }
+    }
+}
---
+++ Services/UserService/User.Infastructure/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using User.Domain.DTOS;
+using User.Domain.Services;
+
+namespace User.Infastructure.Services
+{
+    public class UserService : IUserService
+    {
+        public UserDTO GetUserByUsername(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return null;
+            }
+            var dto = new UserDTO
+            {
+                Name = "Emre",
+                Surname = "Ekiz",
+                BirthDate = new DateTime(2000, 2, 4),
+                Username = username
+            };
+            var dto1 = new UserDTO
+            {
+                Name = "abc",
+                Surname = "Ekiz",
+                BirthDate = new DateTime(2000, 2, 4),
+                Username = username
+            };
+            return dto;
+        }
+    }
+}
